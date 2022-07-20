c557dacc9e185aabf20f7d8e6d425c557282e34f
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-20 05:28:29 +0300

    commit tree 45c4878d674679b8a0573e9d12e9945d4e2f3875

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList />
+      <ShoppingList/>
     </div>
   );
 }
